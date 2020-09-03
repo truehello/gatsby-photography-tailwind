5d067888bae92e3fe7a86eef4e41507e7bfd105c
truehello <trudelmarc@gmail.com>
2020-09-03 16:18:57 -0700

    corrected casing issue with Layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Link from "gatsby-link"
 
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import AboutImage from "../images/gatsby-astronaut.png"
 
 
